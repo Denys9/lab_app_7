86fd0a9981baa6fdd01b6bb3284da042e54d5090
lozin <lozindenis6@gmail.com>
2022-11-05 11:48:08 +0200

    Create work "№1"

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 def str():
     try:
-        text = input('text - ')
+        counter = 0
+        text = input('Text - ')
         words = text.split()
-        print(words)
         for word in words:
             if text.count(word)>0:
                 origen = text
                 temp = text[0].upper() + text[1:]
                 text = text.replace(origen, temp)
         print(text)
-        res = text.count(',') + text.count('?') + text.count('!') + text.count('!') + text.count('-') + text.count(':')
-        print(f'punctuation marks - {res}')
+        for i in text:
+            if i.isnumeric():
+                counter += 1
+        print(f'Num of digits - {counter}')
+        res = text.count(',') + text.count('?') + text.count('!') + text.count('-') + text.count(':')
+        print(f'Punctuation marks - {res}')
+        res = text.count('!')
+        print(f'Number of exclamation marks - {res}')
     except Exception as ex:
         print(f'Eror information: {ex}')
+
 
 str()
