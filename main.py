c666d0aa0b26d68dbe06a3b07358b4f569b95b82
lozin <lozindenis6@gmail.com>
2022-11-05 11:41:22 +0200

    Create work "№1"

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def str():
     try:
         text = input('text - ')
-        if text.count(text) > 0:
-            origen = text
-            temp = text[0].upper() + text[1:]
-            text = text.replace(origen, temp)
-            print(text)
-
+        words = text.split()
+        print(words)
+        for word in words:
+            if text.count(word)>0:
+                origen = text
+                temp = text[0].upper() + text[1:]
+                text = text.replace(origen, temp)
+        print(text)
+        res = text.count(',') + text.count('?') + text.count('!') + text.count('!') + text.count('-') + text.count(':')
+        print(f'punctuation marks - {res}')
     except Exception as ex:
         print(f'Eror information: {ex}')
 
